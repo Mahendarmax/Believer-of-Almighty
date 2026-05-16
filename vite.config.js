0c5468d2a126f16b2be44173cca0ed69f3d76cba
Shivarathri <mahendar.shivarathri@bms.com>
2026-05-16 16:21:58 +0530

    fix: set base to / for custom domain quranintelugu.in

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Believer-of-Almighty/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
